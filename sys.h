691e04c885c00ad9d05e07fe406901723308e3ae
Skyler Barr <39777622+skyhi179319@users.noreply.github.com>
2022-06-07 10:33:38 -0600

    Main Upload

---
+++ sys.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "math.h"
+#include "input.h"
+#include "convert.h"
+#include "file.h"
+#include <stdlib.h>
+#include "JSON.h"
+class sys {
+  public:
+    // imports all header class
+    math math;
+    input input;
+    convert convert;
+    file file;
+    JSON JSON;
+    // console methods
+    void log(std::string x){
+        std::cout << x;
+        std::cout << "\n";
+    }
+    void log(int x){
+      std::cout << x;
+      std::cout << "\n";
+    }
+    // array
+    void printArray(std::string arr[]){
+        for (int i = 0; i < 4; i++) {
+            std::cout << arr[i] << "\n";
+        }
+    }
+    // create line for console
+    void createLine(int x){
+        std::string line;
+        for (int i = 0; i < x; i++) {
+            line.append("-");
+        }
+        std::cout << line;
+        std::cout << "\n";
+    }
+};
